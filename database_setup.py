ab1bd89f800aef26931c9abadb493c6f47b91399
Soyrizo <spencerstenger+github@gmail.com>
2019-04-12 07:53:17 -0700

    fix: format all py files to be pep8 compliant

--- database_setup.py
+++ database_setup.py
@@ -2,48 +2,46 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
- 
+
+
 Base = declarative_base()
+
 
 class Sport(Base):
     __tablename__ = 'sport'
-   
+
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     username = Column(String(250), nullable=True)
 
     @property
     def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'name'         : self.name,
-           'id'           : self.id,
-           'username'     : self.username,
-       }
- 
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+            'username': self.username,
+        }
+
+
 class Item(Base):
     __tablename__ = 'item'
 
-
-    name =Column(String(80), nullable = False)
-    id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
     description = Column(String(250))
-    sport_id = Column(Integer,ForeignKey('sport.id'))
+    sport_id = Column(Integer, ForeignKey('sport.id'))
     sport = relationship(Sport)
-
 
     @property
     def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'name'         : self.name,
-           'description'         : self.description,
-           'id'         : self.id,
-       }
-
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+        }
 
 
 engine = create_engine('sqlite:///sportscatalog.db')
- 
-
 Base.metadata.create_all(engine)
